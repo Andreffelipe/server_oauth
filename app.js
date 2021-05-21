1399a310999ed3fb30c664d4e9e53ad7451dc6f7
Andreffelipe <felipe.andre35@yahoo.com.br>
2021-05-21 11:34:17 -0300

    fix: node sass

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware-5');
+//const sassMiddleware = require('node-sass-middleware-5');
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const env = require('./env');
@@ -22,15 +22,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
   secret: env.salt,
-  cookie: { maxAge: 60000 }
+  cookie: { maxAge: 60000 },
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-}));
+// app.use(sassMiddleware({
+//   src: path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   indentedSyntax: false, // true = .sass and false = .scss
+//   sourceMap: true
+// }));
 app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(proxy)
